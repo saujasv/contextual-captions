abb27f0db4cdfbf9997029289e70e98bf9a33f94
Jing Yu Koh <kohjingyu95@gmail.com>
2024-04-24 16:16:42 -0400

    Add ssl workaround, print metrics

--- evaluate_imagecode.py
+++ evaluate_imagecode.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import torch
 import json
 from tqdm import tqdm
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context  # Workaround for SSL error
 
 
 def evaluate(
@@ -49,7 +51,7 @@ if __name__ == "__main__":
     ]
 
     model, _, preprocess = open_clip.create_model_and_transforms(
-        "ViT-B-32", pretrained="clip-vit-base-patch32/pytorch_model.bin", device="cuda:0", precision="bf16"
+        "ViT-B-32", pretrained="openai", device="cuda:0", precision="bf16"
     )
     tokenizer = open_clip.get_tokenizer("ViT-B-32")
     listener = CLIPListener(model, preprocess, tokenizer, device="cuda:0")
@@ -61,3 +63,4 @@ if __name__ == "__main__":
         evaluation_functions,
         "/data/tir/projects/tir3/users/svadugur/pragmatic-clip/image-sets",
     )
+    print(metrics)
