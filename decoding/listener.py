3f92beadb963ca8268be063b7125eeecb6996429
Jing Yu Koh <kohjingyu95@gmail.com>
2024-04-26 14:29:49 -0400

    Add listener_energy function

--- decoding/listener.py
+++ decoding/listener.py
@@ -6,7 +6,8 @@ import torchvision.transforms.transforms as T
 from PIL import Image
 from einops import rearrange
 import itertools
-
+from transformers import Blip2ForConditionalGeneration, Blip2Processor
+import torch.nn.functional as F
 
 class Listener:
     def encode_images(self, images: Union[List[Path], List[str]]):
@@ -124,3 +125,83 @@ class CLIPListener(Listener):
             .mean()
             .item()
         )
+    
+    def listener_energy(contexts: List[Image.Image], target: int,
+                        processor: Blip2Processor, captioning_model: Blip2ForConditionalGeneration, 
+                        input_ids: torch.Tensor, input_embeds: torch.Tensor) -> torch.Tensor:
+        """Computes the log likelihood of the input_embeds given the image contexts.
+
+        Args:
+            contexts (List[Image.Image]): A list of images.
+            target (int): Index of contexts to use.
+            captioning_model (Blip2ForConditionalGeneration): The captioning model.
+            input_embeds (torch.Tensor): The input embeddings.
+
+        Returns:
+            torch.Tensor: Log likelihoods for each context.
+
+        Example usage:
+        ```
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+            model = Blip2ForConditionalGeneration.from_pretrained(
+                "Salesforce/blip2-opt-2.7b", load_in_8bit=False, device_map={"": 0}, torch_dtype=torch.float16
+            )
+
+            url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+            image = Image.open(requests.get(url, stream=True).raw)
+            contexts = [image, image]
+
+            prompts = ["There are two cats in this image", "There are two dogs in this image"]
+            input_ids = [processor(images=None, text=prompt, return_tensors="pt").input_ids.to(device=device) for prompt in prompts]
+            input_ids = torch.cat(input_ids, dim=0)
+            # TODO: Replace this with the actual embeddings
+            inputs_embeds = model.language_model.get_input_embeddings()(input_ids)
+            le = listener_energy(contexts, 0, processor, model, input_ids, inputs_embeds)
+            le.backward()
+            inputs_embeds.grad
+        ```
+        """
+        input_embeds.retain_grad()
+        pixel_values = torch.cat([processor(images=context, return_tensors="pt")['pixel_values'].to(device=device, dtype=captioning_model.dtype) for context in contexts], dim=0)
+        vision_outputs = captioning_model.vision_model(
+            pixel_values=pixel_values,
+            output_attentions=True,
+            output_hidden_states=True,
+            return_dict=False
+        )
+        image_embeds = vision_outputs[0]
+        
+        image_attention_mask = torch.ones(image_embeds.size()[:-1], dtype=torch.long, device=image_embeds.device)
+        query_tokens = captioning_model.query_tokens.expand(image_embeds.shape[0], -1, -1)
+        query_outputs = captioning_model.qformer(
+            query_embeds=query_tokens,
+            encoder_hidden_states=image_embeds,
+            encoder_attention_mask=image_attention_mask,
+            output_attentions=False,
+            output_hidden_states=False,
+            return_dict=False,
+        )
+        query_output = query_outputs[0]  # (2, 32, 768)
+        
+        language_model_inputs = captioning_model.language_projection(query_output)
+        language_model_attention_mask = torch.ones(
+            language_model_inputs.size()[:-1], dtype=torch.long, device=language_model_inputs.device
+        )
+        final_inputs_embeds = torch.cat([language_model_inputs, input_embeds.to(language_model_inputs.device)], dim=1)
+        
+        attention_mask = torch.ones_like(input_ids)
+        attention_mask = torch.cat([language_model_attention_mask, attention_mask.to(language_model_attention_mask.device)], dim=1)
+        
+        outputs = captioning_model.language_model(
+            inputs_embeds=final_inputs_embeds,
+            attention_mask=attention_mask,
+            output_attentions=False,
+            output_hidden_states=False,
+            return_dict=True
+        )
+        
+        log_probs = F.log_softmax(outputs.logits[:, 32:, :], dim=-1)
+        actual_log_probs = torch.gather(log_probs, 2, input_ids.unsqueeze(-1)).squeeze(-1)
+        log_likelihood = actual_log_probs.sum(dim=-1)
+        return log_likelihood[target]
